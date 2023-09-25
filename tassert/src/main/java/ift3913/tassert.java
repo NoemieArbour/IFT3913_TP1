3dd0630e2817cd1df7fdd186f4264d04e5dcc59d
Noémie Arbour <77739955+NoemieArbour@users.noreply.github.com>
2023-09-25 17:05:28 -0400

    Completed first version of tassert (tests needed)

--- tassert/src/main/java/ift3913/tassert.java
+++ tassert/src/main/java/ift3913/tassert.java
@@ -1,11 +1,44 @@
 package ift3913;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * {@link tassert} est un programme qui, étant donné un fichier source d'une classe de test java, calcule la métrique de
  * taille TASSERT : nombre de assertions JUnit.
  */
 public class tassert {
     public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("TASSERT must contain 1 argument: java tassert <filename>.java");
+            System.exit(1);
+        }
 
+        String fileName = args[0];
+        int assertCount = countAsserts(fileName);
+
+        System.out.println(assertCount);
+    }
+
+    private static int countAsserts(String fileName) {
+        int count = 0;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            Pattern assertPattern = Pattern.compile("(assertTrue|assertFalse|assertEquals|assertNotEquals|" +
+                    "assertArrayEquals|assertNotNull|assertNull|assertSame|assertNotSame|assertThat|assertThrows)(\\()");
+
+            while((line = reader.readLine()) != null) {
+                Matcher matcher = assertPattern.matcher(line);
+                while(matcher.find()) count++;
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return count;
     }
 }
