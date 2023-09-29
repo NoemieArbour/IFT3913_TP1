4a4983424cdcf06f3a37340cfad5e5867cd7085e
nhungdang <nhungdth88@gmail.com>
2023-09-29 19:34:12 -0400

    Troncomp print out command line
    Troncomp print out with command line

--- tropcomp/src/main/java/ift3913/tropcomp.java
+++ tropcomp/src/main/java/ift3913/tropcomp.java
@@ -2,6 +2,7 @@ package ift3913;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -13,8 +14,6 @@ public class tropcomp {
 
     private static ArrayList<String> streamOutTLS = new ArrayList<String>();
 
-    // private static ArrayList<String>[] tlocDescendingOrder;
-    // private static String[] tmcpDescendingOrder;
     public static void main(String[] args) {
         File maindir;
         switch (args.length) {
@@ -30,7 +29,7 @@ public class tropcomp {
                         System.exit(1);
                     } else {
                         streamOutTLS = executeJarFile(args[1]);
-                        suspectedClasses(args[1], Integer.parseInt(args[2]));
+                        suspectedClasses(args[1], "", Integer.parseInt(args[2]), 1);
                     }
                 }
                 break;
@@ -40,58 +39,76 @@ public class tropcomp {
                     System.err.println("Project not found! Please verify your entry path.");
                     System.exit(1);
                 } else {
-                    // System.out.println("\nWorking..........\n");
-                    // suspectedClasses(2, args[1]);
-
+                    System.out.println("\nWorking..........\n");
+                    streamOutTLS = executeJarFile(args[3]);
+                    suspectedClasses(args[1], null, Integer.parseInt(args[2]), 1);
+                    suspectedClasses(args[4], args[3], Integer.parseInt(args[5]), 2);
                 }
                 break;
             default:// invalid entry
                 System.err.println(
-                        "troncomp must contain at least 2 arguments: java troncomp <project_path> <threshold(e.g.1,5,10,20)> OR \n java troncomp -o <output_path.csv> <entry_path>");
+                        "tropcomp must contain at least 2 arguments: java tropcomp <project_path> <threshold(e.g.1,5,10,20)> OR \n java tropcomp -o <output_path.csv> <entry_path> <threshold(e.g.1,5,10,20)>");
                 break;
         }
     }
 
-    private static void suspectedClasses(String path, int seuil) {
+    private static void suspectedClasses(String inputPath, String outputPath, int seuil, int typeSortie) {
+
         ArrayList<String> tlocDescendingOrder = new ArrayList<>(streamOutTLS);
         ArrayList<String> tmcpDescendingOrder = new ArrayList<>(streamOutTLS);
         sort(tlocDescendingOrder, 3);
         sort(tmcpDescendingOrder, 5);
-        int totalOfClass = streamOutTLS.size();
-        System.out.println("%%%%%%%%%%%%%%%%%%%TLOC ");
-        for (int i = 0; i < tlocDescendingOrder.size(); i++) {
-            System.out.println(tlocDescendingOrder.get(i).split(", ")[2]);
-            System.out.print("-- tloc: " + tlocDescendingOrder.get(i).split(", ")[3]);
-            System.out.print("-- tcmp: " + tlocDescendingOrder.get(i).split(", ")[5]);
-            System.out.print("\n");
-        }
-        System.out.println("%%%%%%%%%%%%%%%%%%%TCMP ");
-        for (int i = 0; i < tmcpDescendingOrder.size(); i++) {
-            System.out.println(tmcpDescendingOrder.get(i).split(", ")[2]);
-            System.out.print("-- tloc: " + tmcpDescendingOrder.get(i).split(", ")[3]);
-            System.out.print("-- tcmp: " + tmcpDescendingOrder.get(i).split(", ")[5]);
-            System.out.print("\n");
-        }
-
-        System.out.println("%%%%%%%%%%%%%%%%%%%SAMEEE ");
+        // System.out.println("%%%%%%%%%%%%%%%%%%%TLOC ");
+        // for (int i = 0; i < 72; i++) {
+        // System.out.print(tlocDescendingOrder.get(i).split(", ")[2]);
+        // System.out.print("-- tloc: " + tlocDescendingOrder.get(i).split(", ")[3]);
+        // System.out.print("-- tcmp: " + tlocDescendingOrder.get(i).split(", ")[5]);
+        // System.out.print("\n");
+        // }
+        // System.out.println("%%%%%%%%%%%%%%%%%%%TCMP ");
+        // for (int i = 0; i < 72; i++) {
+        // System.out.print(tmcpDescendingOrder.get(i).split(", ")[2]);
+        // System.out.print("-- tloc: " + tmcpDescendingOrder.get(i).split(", ")[3]);
+        // System.out.print("-- tcmp: " + tmcpDescendingOrder.get(i).split(", ")[5]);
+        // System.out.print("\n");
+        // }
         int step = streamOutTLS.size() * seuil / 100;
-        System.out.println("############## " + step);
-        System.out.println("############## " + totalOfClass);
+        // System.out.println("############## " + step);
+        // System.out.println("############## " + streamOutTLS.size());
+        // System.out.println("%%%%%%%%%%%%%%%%%%%SAMEEE ");
+        ArrayList<String> getSameClasses = getSameObjects(tmcpDescendingOrder, tlocDescendingOrder, step);
+        if (typeSortie == 1) {// printout command line
+            for (int i = 0; i < getSameClasses.size(); i++) {
+                System.out.println(getSameClasses.get(i));
+            }
+        } else { // csv file
+
+            // try {
+            // File csvFile = new File(outputPath);
+            // FileWriter fileWriter = new FileWriter(csvFile);
+            // StringBuilder line = new StringBuilder();
+            // for (int i = 0; i < getSameClasses.size(); i++) {
+            // line.append(getSameClasses.get(i)).append("\n");
+            // }
+            // fileWriter.write(line.toString());
+            // fileWriter.close();
+            // } catch (IOException exception) {
+            // exception.printStackTrace();
+            // }
+        }
+    }
+
+    private static ArrayList<String> getSameObjects(ArrayList<String> ls1, ArrayList<String> ls2, int step) {
+        ArrayList<String> temp = new ArrayList<String>();
         for (int i = 0; i < step; i++) {
-            String find = tmcpDescendingOrder.get(i).split(", ")[2];
+            String find = ls1.get(i).split(", ")[2];
             for (int j = 0; j < step; j++) {
-                if (find.equals(tlocDescendingOrder.get(j).split(", ")[2])) {
-                    System.out.println(tlocDescendingOrder.get(j));
+                if (find.equals(ls2.get(j).split(", ")[2])) {
+                    System.out.println(ls2.get(j));
                 }
             }
         }
-
-        // if(typeSortie == 1){// printout command line
-
-        // }else { // csv file
-
-        // }
-
+        return temp;
     }
 
     private static ArrayList<String> sort(ArrayList<String> strArr, int idxComp) {
@@ -112,7 +129,7 @@ public class tropcomp {
                     if (!isNumeric(fst) && !isNumeric(snd)) {// if 2 infinity=>check tloc
                         fst = strArr.get(j).split(", ")[3];
                         snd = strArr.get(min_idx).split(", ")[3];
-                        if (fst.compareTo(snd) > 0) {
+                        if (Integer.parseInt(fst) > Integer.parseInt(snd)) {
                             min_idx = j;
                         }
                     } else {
@@ -137,7 +154,8 @@ public class tropcomp {
         ArrayList<String> temp = new ArrayList<>();
         String[] aStrings = new String[] { "java", "-jar", "tls.jar", path };
         ProcessBuilder processBuilder = new ProcessBuilder(aStrings);
-        processBuilder.redirectError(new File(Paths.get("extJar_out_put.txt").toString()));
+        // processBuilder.redirectError(new
+        // File(Paths.get("extJar_out_put.txt").toString()));
         processBuilder.redirectInput();
         try {
             final Process process = processBuilder.start();
